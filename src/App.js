a5745a48e69c8c7d456cf6f093353f875bd95ce6
iamRajatKumar <rohit.r.sharma2@gmail.com>
2024-10-21 23:55:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Clock from './components/clock';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[time, setTime] = useState(new Date());
+
+  useEffect (()=> {
+    setInterval(()=>setTime(new Date()),1000)
+  },[])
+
   return (
     <div className="App">
+      <p className='p'>{time.toLocaleTimeString()}</p>
       <Clock/>
     </div>
   );
